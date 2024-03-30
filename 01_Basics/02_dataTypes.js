75fd76fbdb74fc840d3206ddcd22b4209447900f
Ankit-2001 <76491445+Ankit-2001@users.noreply.github.com>
2024-03-30 11:15:57 +0000

    Datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version through out;
+
+// alert (3+3) we can use this only in browser.
+
+let name = "Ankit" // string
+let age = 22 //number
+let isLoggedIn = false //Boolean
+
+/* Primitive Data type 
+1.number => 2 to power 53
+2.bigint
+3.string => ""
+4.boolean => true/false
+5.null => standalone value
+6.undefined => without assigning value
+7.symbol => finding uniqueness.
+*/
+
+//Non-primitive
+//Array
+//Object
+console.log(typeof undefined) //undefined
+console.log(typeof null); //object
