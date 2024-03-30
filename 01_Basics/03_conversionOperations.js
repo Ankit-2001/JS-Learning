d4edbc7b27098658444e76142072b222d9ad6f96
Ankit-2001 <76491445+Ankit-2001@users.noreply.github.com>
2024-03-30 11:33:50 +0000

    conversionOperations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "40";
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(typeof (valueInNumber));
+
+//"33" => 33
+//"33abc" => NaN
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+
+let someNumber = 44;
+let stringNumber = String(someNumber);
+
+
+// ******* OPERATIONS *****
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
