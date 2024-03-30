13f6af148e551185946a288746a08918d9447eab
Ankit-2001 <76491445+Ankit-2001@users.noreply.github.com>
2024-03-30 11:06:02 +0000

    About variable

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 100; // value will remain fixed throught the code
+
+let accountEmail = "ankitb9472@gmail.com"
+var accountPassword = "12345";
+accountCity = "Darbhanga";
+
+/*
+Prefer not to use var 
+because of issue in block scope and function.
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
