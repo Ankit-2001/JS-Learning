4ae3eaecdbbb6a4b3df1e200639e933292d3bb9f
Ankit-2001 <76491445+Ankit-2001@users.noreply.github.com>
2024-04-02 06:38:13 +0000

    Different array methods

---
+++ 02_Basics/02_array.js
@@ -0,0 +1,26 @@
+const marvel_heros = ["Thor","Ironman","spiderman"];
+const dc_heros = ["superman","flash","batman"];
+
+//marvel_heros.push(dc_heros);
+//console.log(marvel_heros);
+
+//arr1.concat(arr2) -> add values of arr2 into arr1 and return a new array
+
+// const all_heros = marvel_heros.concat(dc_heros);
+// console.log(all_heros);
+
+
+//spread operator -> break all the value into fragments type 
+// const all_new_heros = [...marvel_heros,...dc_heros];
+// console.log(all_new_heros);
+
+
+//arr.flat(n) -> return our nested array by converting into another array by going till depth of n
+
+//Array.isArray(data) ->check if data is array or not and return boolean value
+
+//Array.from(data) -> convert data into array if possible and if not return  an empty array.
+//console.log(Array.from("Ankit kumar bhardwaj"));
+
+
+//Array.of(val1,val2,val3) -> convert the all the val1,val2,val3 in single array.
