31089ef4cc432171234b20b3b5f3b3bc9eb0d12c
Ankit-2001 <76491445+Ankit-2001@users.noreply.github.com>
2024-04-02 05:16:32 +0000

    Different string methods

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,19 @@
+const name = "Ankit";
+const age = 22;
+
+//string interpolation
+console.log(`Hello my name is ${name} and my age is ${age}.`);
+
+//indexOf -> first occurence of char in string;
+
+//substring(st,end) -> [st , end);
+
+//slice(st,end) -> [st,end); 
+
+//trim() -> remove starting and ending space from string if any.
+
+//replace('search','replace') -> replace search with replace in any string.
+
+//include(val) -> check val is present in string or not and return boolean value.
+
+//split(split_criteria) -> split string in many parts according to split_criteria.
