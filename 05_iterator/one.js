5fcf223fe2cbdf18fa22d53b3b64869da13a56ad
Ankit-2001 <76491445+Ankit-2001@users.noreply.github.com>
2024-04-03 10:37:55 +0000

    Loops

---
+++ 05_iterator/one.js
@@ -0,0 +1,26 @@
+//for
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    //console.log(index);
+}
+
+
+//break and continue
+//break -> comes out of loop
+//continue -> skip all part of code after continue statement
+
+//while
+let i = 0
+while (i<10) {
+    //console.log(i);
+    i++;
+}
+
+
+//do{....}while(); -> minimum 1 execution.
+i = 0;
+do {
+    //console.log(i);
+    i++;
+} while (i<10);
