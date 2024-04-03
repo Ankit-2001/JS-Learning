5de9a91c15dff2bc7daae0c9f434b05d6e180ed0
Ankit-2001 <76491445+Ankit-2001@users.noreply.github.com>
2024-04-03 10:19:55 +0000

    Basic Control Flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,40 @@
+//if
+const isUserLoggedIn = true;
+// if(isUserLoggedIn){
+//     console.log(`userLoggedIn: ${isUserLoggedIn}`)
+// }else{
+//     console.log(`userLoggedIn: ${isUserLoggedIn}`);
+// }
+
+// <,>,<=,>= ,==(value check),!=, ===(value as well as type check)
+
+// ****** SWITCH *****//
+ /*switch(key){
+    case value:
+
+        break;
+    case value:
+
+        break;
+    default:
+        break;
+ }*/
+
+    
+ //Falsy value
+ /*
+ "",false,NaN,undefined,NULL,0;
+ */
+
+ // Nullish coalescing operator (??): null undefined
+
+// ?? check null/undefined  safety.
+// let val1;
+// val1 = null ?? 15; // Output 15;
+// val1 = undefined ?? 10; //Output 10;
+// console.log(val1);
+
+
+//Terneary  Operator
+//condition ? statement1 : statement2;
+//if condtiont satisfies statement1 otherwise execute statement2
